f44f3a1e01967286b91b55386fecd192cc567cfd
sumiship <osumi.k.osumi.k@gmail.com>
2021-05-19 04:49:52 +0900

    baseSystem completed

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="home">
+    <Header/>
+    <Field />
+    <Footer/>
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Field from '@/components/Field.vue';
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+
+@Component({
+  components: {
+    Field,
+    Header,
+    Footer
+  },
+})
+export default class Home extends Vue {}
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
 }
 </style>
+
